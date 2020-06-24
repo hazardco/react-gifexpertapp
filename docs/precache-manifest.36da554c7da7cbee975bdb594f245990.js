7b3345b47b79ba849daaec171309f82dc0582175
Luis Miguel Cabezas <luismiguel.cabezas@gmail.com>
2020-06-24 13:27:12 +0200

    docs agregado

---
+++ docs/precache-manifest.36da554c7da7cbee975bdb594f245990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82be71107894c7573623ee219fe4d1d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "753033dbec574d729fab",
+    "url": "/static/css/main.b01c75aa.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753033dbec574d729fab",
+    "url": "/static/js/main.ccb49b67.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
